4f608007fb93ed18a05162c8821cb7db7b8757fc
Shahrul Syazwan <syzwnngwn@gmail.com>
2024-10-02 12:46:50 +0800

    modified ApproveUsers.php

--- app/Http/Livewire/Admin/ApproveUsers.php
+++ app/Http/Livewire/Admin/ApproveUsers.php
@@ -7,38 +7,6 @@ use App\Models\User;
 
 class ApproveUsers extends Component
 {
-<<<<<<< HEAD
-
-    public $user_id;
-
-    public function approve(User $user){
-        $this->user_id = $user->id;
-
-    }
-    public function confirmApprove() {
-        $user = User::find($this->user_id);
-        $user->assignRole('staff');
-        $user->removeRole('user');
-        session()->flash('success', 'User '.$user->name.' approved successfully!');
-    }
-
-
-    public function reject(User $user){
-
-        $user->update([
-            'reject_status'=>1,
-        ]);
-
-    }
-
-    public function render()
-    {
-        $users = User::role('user')->get();
-        //dd($users);
-        return view('livewire.admin.approve-users', compact('users'));
-    }
-}
-=======
     public $selectedUserId;
     public $actionType; // To track whether approving or rejecting
 
@@ -86,4 +54,3 @@ class ApproveUsers extends Component
         }
     }
 }
->>>>>>> 79a5615aa9faf2b90247f9c6c49ca66143ffe5d6
