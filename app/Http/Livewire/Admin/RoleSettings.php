e9b97e78691d8e34a713b158fa39ff5a3c13a8bc
farraaqila <farra2001@graduate.utm.my>
2024-11-27 12:04:07 +0800

    rolesettings

--- app/Http/Livewire/Admin/RoleSettings.php
+++ app/Http/Livewire/Admin/RoleSettings.php
@@ -1,74 +1,4 @@
 <?php
-
-namespace App\Http\Livewire\Admin;
-
-use Illuminate\Http\Request;
-use Livewire\Component;
-use Spatie\Permission\Models\Role;
-
-class RoleSettings extends Component
-{
-
-    public function render()
-    {
-        $allRolesInDatabase = Role::all();
-
-        return view('livewire.admin.role-settings.show', ['allRolesInDatabase' => $allRolesInDatabase]);
-    }
-
-    public function index()
-    {
-        return $this->render();
-    }
-
-    public function store(Request $request){
-        $request->validate([
-            'name' => 'required|string|max:20',
-            'desc' => 'required|string|min:5',
-            'permissions' => 'array',
-            'permissions.*' => 'string',
-        ]);
-
-        $role = Role::create([
-            'name' => $request->name,
-            'desc' => $request->desc
-        ]);
-
-        $role->syncPermissions($request->permissions);
-
-        return redirect()->route('admin.role')->with('success', 'Role created successfully');
-
-    }
-
-    public function update(Request $request, $id)
-    {
-        //dd($request->all());
-        $request->validate([
-            'name' => 'required|string|max:20',
-            'desc' => 'required|string|min:5',
-            'permissions' => 'array',
-            'permissions.*' => 'string',
-        ]);
-
-
-        $role = Role::findOrFail($id);
-        $role->name = $request->name;
-        $role->desc = $request->desc;
-        $role->save();
-
-        $role->syncPermissions($request->permissions);
-
-        return redirect()->route('admin.role')->with('success', 'Role updated successfully');
-    }
-
-    public function delete($id){
-        $role = Role::findOrFail($id);
-        $role->delete();
-
-        return redirect()->route('admin.role')->with('success', 'Role deleted successfully');
-
-    }
-
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
@@ -147,5 +77,4 @@ class RoleSettings extends Component
             'roles' => $this->roles,
         ]);
     }
-
 }
