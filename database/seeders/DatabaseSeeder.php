3ccc059bc5d9ddd44744618fa77036f3bf65cf3a
Mohd Aidi <mohd-aidi@users.noreply.github.com>
2025-01-03 02:24:24 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         User::factory()->create([
             'name' => 'admin',
-            'email' => 'admin@softui.com',
+            'email' => 'admin@kodewave.my',
             'password' => Hash::make('secret')
         ]);
     }
