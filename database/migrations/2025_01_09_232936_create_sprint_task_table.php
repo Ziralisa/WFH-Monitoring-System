cf7472f24f0ff6b46ed6e79f3055fd5542f400d3
farraaqila <farra2001@graduate.utm.my>
2025-01-10 16:39:04 +0800

    update migration

--- database/migrations/2025_01_09_232936_create_sprint_task_table.php
+++ database/migrations/2025_01_09_232936_create_sprint_task_table.php
@@ -15,20 +15,18 @@ return new class extends Migration {
             $table->unsignedBigInteger('project_id');
             $table->unsignedBigInteger('sprint_id');
             $table->unsignedBigInteger('task_id');
-            $table->enum('task_status', ['to do', 'in progress', 'done', 'stuck'])->default('to do');
-            $table->enum('task_priority', ['low', 'medium', 'high'])->default('medium');
-            $table->unsignedBigInteger('task_assign')->nullable(); // ID of the assigned user
             $table->timestamps();
-
+    
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
             $table->foreign('sprint_id')->references('id')->on('sprints')->onDelete('cascade');
-            $table->foreign('task_id')->references('id')->on('project_task')->onDelete('cascade');
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
         });
     }
-
+    
     public function down()
     {
         Schema::dropIfExists('sprint_task');
     }
+    
 
 };
